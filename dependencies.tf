b98b1cf37ddd26a3f73e80c5b5af56986a58a27b
Earl Wilson <ewilwon57@gmail.com>
2020-03-24 13:32:23 -0400

    Added project files"

---
+++ dependencies.tf
@@ -0,0 +1,74 @@
+locals {
+  virtual_machine_name = "${var.prefix}-vm"
+  admin_username       = "testadmin"
+  admin_password       = "Password1234!"
+}
+
+resource "azurerm_resource_group" "compute" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "compute" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.compute.location
+  resource_group_name = azurerm_resource_group.compute.name
+}
+
+resource "azurerm_network_security_group" "compute" {
+  name                = "linux_base"
+  location            = azurerm_resource_group.compute.location
+  resource_group_name = azurerm_resource_group.compute.name
+
+  security_rule {
+    name              = "ssh_http"
+    priority          = 100
+    direction         = "Inbound"
+    access            = "Allow"
+    protocol          = "Tcp"
+    source_port_range = "*"
+    destination_port_ranges = [
+      "22",
+      "80"
+    ]
+    source_address_prefix      = "192.180.173.26/32"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Test"
+  }
+}
+
+resource "azurerm_subnet" "compute" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.compute.name
+  virtual_network_name = azurerm_virtual_network.compute.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_subnet_network_security_group_association" "compute" {
+  subnet_id                 = azurerm_subnet.compute.id
+  network_security_group_id = azurerm_network_security_group.compute.id
+}
+
+resource "azurerm_public_ip" "compute" {
+  name                = "${var.prefix}-publicip"
+  resource_group_name = azurerm_resource_group.compute.name
+  location            = azurerm_resource_group.compute.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "compute" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.compute.location
+  resource_group_name = azurerm_resource_group.compute.name
+
+  ip_configuration {
+    name                          = "configuration"
+    subnet_id                     = azurerm_subnet.compute.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.compute.id
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix used for all resources"
+  default     = "compute2"
+}
+
+variable "location" {
+  description = "The Azure Region in which the resources should exist"
+  default     = "East US 2"
+}
