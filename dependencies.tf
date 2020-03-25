41933c3013dac36787c84314c9bafcd171258556
Earl Wilson <ewilson7@gmail.com>
2020-03-25 11:58:03 -0400

    Refactored, added shutdown schedule

--- dependencies.tf
+++ dependencies.tf
@@ -1,19 +1,19 @@
-resource "azurerm_resource_group" "compute" {
+resource "azurerm_resource_group" "compute2" {
   name     = "${var.prefix}-rg"
   location = var.location
 }
 
-resource "azurerm_virtual_network" "compute" {
+resource "azurerm_virtual_network" "compute2" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.compute.location
-  resource_group_name = azurerm_resource_group.compute.name
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.compute2.location
+  resource_group_name = azurerm_resource_group.compute2.name
 }
 
-resource "azurerm_network_security_group" "compute" {
+resource "azurerm_network_security_group" "compute2" {
   name                = "linux_base"
-  location            = azurerm_resource_group.compute.location
-  resource_group_name = azurerm_resource_group.compute.name
+  location            = azurerm_resource_group.compute2.location
+  resource_group_name = azurerm_resource_group.compute2.name
 
   security_rule {
     name              = "ssh_http"
@@ -35,34 +35,34 @@ resource "azurerm_network_security_group" "compute" {
   }
 }
 
-resource "azurerm_subnet" "compute" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.compute.name
-  virtual_network_name = azurerm_virtual_network.compute.name
-  address_prefix       = "10.0.2.0/24"
+resource "azurerm_subnet" "compute2" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.compute2.name
+  virtual_network_name = azurerm_virtual_network.compute2.name
+  address_prefix       = "10.1.1.0/24"
 }
 
-resource "azurerm_subnet_network_security_group_association" "compute" {
-  subnet_id                 = azurerm_subnet.compute.id
-  network_security_group_id = azurerm_network_security_group.compute.id
+resource "azurerm_subnet_network_security_group_association" "compute2" {
+  subnet_id                 = azurerm_subnet.compute2.id
+  network_security_group_id = azurerm_network_security_group.compute2.id
 }
 
-resource "azurerm_public_ip" "compute" {
+resource "azurerm_public_ip" "compute2" {
   name                = "${var.prefix}-publicip"
-  resource_group_name = azurerm_resource_group.compute.name
-  location            = azurerm_resource_group.compute.location
+  resource_group_name = azurerm_resource_group.compute2.name
+  location            = azurerm_resource_group.compute2.location
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_interface" "compute" {
+resource "azurerm_network_interface" "compute2" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.compute.location
-  resource_group_name = azurerm_resource_group.compute.name
+  location            = azurerm_resource_group.compute2.location
+  resource_group_name = azurerm_resource_group.compute2.name
 
   ip_configuration {
     name                          = "configuration"
-    subnet_id                     = azurerm_subnet.compute.id
+    subnet_id                     = azurerm_subnet.compute2.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.compute.id
+    public_ip_address_id          = azurerm_public_ip.compute2.id
   }
 }
--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ locals {
   virtual_machine_name = "${var.prefix}-vm"
 }
 
-resource "azurerm_virtual_machine" "compute" {
+resource "azurerm_virtual_machine" "compute2" {
   name                = local.virtual_machine_name
-  location            = azurerm_resource_group.compute.location
-  resource_group_name = azurerm_resource_group.compute.name
+  location            = azurerm_resource_group.compute2.location
+  resource_group_name = azurerm_resource_group.compute2.name
 
-  network_interface_ids = [azurerm_network_interface.compute.id]
+  network_interface_ids = [azurerm_network_interface.compute2.id]
   vm_size               = "Standard_D1_v2"
 
   # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
@@ -43,5 +43,28 @@ resource "azurerm_virtual_machine" "compute" {
       path     = "/home/${var.admin_username}/.ssh/authorized_keys"
       key_data = var.ssh_key
     }
+  }
+}
+
+resource "azurerm_dev_test_lab" "compute2" {
+  name                = "shutdown-computevm-${local.virtual_machine_name}"
+  location            = azurerm_resource_group.compute2.location
+  resource_group_name = azurerm_resource_group.compute2.name
+}
+
+resource "azurerm_dev_test_schedule" "compute2" {
+  name                = "${local.virtual_machine_name}-AutoStop"
+  location            = azurerm_resource_group.compute2.location
+  resource_group_name = azurerm_resource_group.compute2.name
+  lab_name            = azurerm_dev_test_lab.compute2.name
+
+  daily_recurrence {
+    time      = "2300"
+  }
+
+  time_zone_id = "Eastern Standard Time"
+  task_type    = "LabVmsShutdownTask"
+
+  notification_settings {
   }
 }
